813ab294ea4f1bebcdc2bb4c7d11cbbb58530f3b
landon burnier <landonburnier@gmail.com>
2016-08-30 15:53:00 -0400

    morestuff

--- app.js
+++ app.js
@@ -22,31 +22,64 @@
 
 
 
-let products = [
-    { name: 'Soap dispenser', price: 12.45, inStock: true },
-    { name: 'Chainsaw blade', price: 41.45, inStock: true },
-    { name: 'Bath towel warmer', price: 87.14, inStock: false },
-];
+// let products = [
+//     { name: 'Soap dispenser', price: 12.45, inStock: true },
+//     { name: 'Chainsaw blade', price: 41.45, inStock: true },
+//     { name: 'Bath towel warmer', price: 87.14, inStock: false },
+// ];
 
 
 
-let g = products.filter(stuff);
+// let g = products.filter(stuff);
 
-let j = g.map(smoosh);
+// let j = g.map(smoosh);
 
 
-function stuff(products){
-    if(products.inStock === true){
-        return true;
-    }else{
-        return false;
-    }
+// function stuff(products){
+//     if(products.inStock === true){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+// function smoosh(x){
+//     let y = x.name + ' ' + x.price;
+//     return y;
+
+// }
+// console.log(j)
+
+
+
+
+
+
+
+let input = 153;
+// x = numbers.split / array of numbers 
+// then write function that powers those individual numbers by the length 
+// of the array. 
+
+let g = split(input).map(tonumber).map(power).reduce(combine);
+
+function split(input){
+    let x = input.toString();
+    return x.split('');
 }
 
-function smoosh(x){
-    let y = x.name + ' ' + x.price;
+function tonumber(x){
+    let y = Number(x);
     return y;
+}
+
+function power(k){
+    return Math.pow(k, split(input).length);
+}
+function combine(powered){
+   return Math.sum(spaghetti);
 
 }
-console.log(j)
 
+
+console.log(g);
